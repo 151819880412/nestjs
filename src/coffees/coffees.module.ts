9ce1d44d2466d3752330ff127803659fcaa07a99
zhaoxin <zhaoxin@tofflon.com>
2022-07-05 16:35:13 +0800

    几种自定义 service 提供者

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,13 +1,29 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '@/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+
+class MockCoffeesService {}
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    return ['aaa', 'bbb', 'ccc'];
+  }
+}
 
 /**
  * imports    让 TypeORM 知道这个子模块本身内部的实体
+ * exports    导出 service 让其他模块跨服务调用
+ * useFactory 允许我们动态创建提供者
  * @date 2022-07-04
  * @param {any} {imports:[TypeOrmModule.forFeature([Coffee]
  * @returns {any}
@@ -15,7 +31,31 @@ import { Event } from '@/events/entities/event.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  // providers: [CoffeesService],
+  // providers: [{ provide: CoffeesService, useClass: CoffeesService }],
+  // providers: [{ provide: CoffeesService, useClass: MockCoffeesService }],
+  providers: [
+    CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'deve;opment'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
+    CoffeeBrandsFactory,
+    // { provide: COFFEE_BRANDS, useValue: ['aaaaa', 'bbbbbb'] },
+    // {
+    //   provide: COFFEE_BRANDS,
+    //   useFactory: () => ['aaa', 'bbb'],
+    // },
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
